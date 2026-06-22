e3db8c69e15c2a4f0bb0a6f6695146126821cabe
jose manuel <rodriguezpenajosemanuel62@gmail.com>
2026-06-22 15:26:16 -0500

    mejorando la comunicacion

--- src/main/java/multiservicioRafael/invenatario/CodigoFuente/ConfigreConect/CorsConfig.java
+++ src/main/java/multiservicioRafael/invenatario/CodigoFuente/ConfigreConect/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         // Cambiamos el "*" por tu URL real de Vercel y tu puerto local para que no falle nada
-                        .allowedOrigins("https://proyecto-multiservicio-rafael.vercel.app/", "http://localhost:4200")
+                        .allowedOrigins("https://proyecto-multiservicio-rafael.vercel.app", "http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
